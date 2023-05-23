e99f018604042935c775b1491d4ab752fd420b2d
Hjalmar-Lundmark <hjalmar.lundmark@elev.ga.ntig.se>
2023-05-23 13:34:15 +0200

    Added admin page

---
+++ routes/admin.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql2');
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+});
+const promisePool = pool.promise();
+
+router.get('/', async function(req, res, next){
+    const [rows] = await promisePool.query("SELECT name, votedOn FROM hl21users2");
+
+    res.render('uList.njk', {
+        title: 'Lista av användare',
+        rows: rows, 
+        user: req.session.user,
+        loggedIn: req.session.LoggedIn,
+        
+    });
+});
+
+module.exports = router;
